f5bd19128b5f513ea21b78d3aed7c68d676cfcf3
busyboyent <busyboyent@gmail.com>
2025-06-06 16:58:18 +0500

    отказ от избыточных drf из интернетных гайдов

--- service/page_parser/urls.py
+++ service/page_parser/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
-from rest_framework.routers import DefaultRouter
 from .views import ParsedPageViewSet
-
-router = DefaultRouter()
-router.register(r'page', ParsedPageViewSet, basename='page')
 
 urlpatterns = [
     path('page/create/', ParsedPageViewSet.as_view({'post': 'create_page'}), name='create-page'),
     path('page/<int:pk>/', ParsedPageViewSet.as_view({'get': 'get_page'}), name='get-page'),
     path('page/list/', ParsedPageViewSet.as_view({'get': 'list_pages'}), name='list-pages'),
-] + router.urls
+]
